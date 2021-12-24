e4485f1c56ed7f0f5d67d74940ffafc6ff1912e0
mercuryblaze <77534657+mercuryblaze@users.noreply.github.com>
2021-12-24 19:53:56 +0300

    Added UI

---
+++ Assets/Scripts/ButtonLog.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLog : MonoBehaviour
+{
+    public void Log()
+    {
+        Debug.Log("Проверка кнопки");
+    }    
+}
